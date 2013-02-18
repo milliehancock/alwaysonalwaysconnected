ae68c4adca076b1f2ee9d31e327fa02de90d9c1f
milliehancock <amelia.a.hancock@gmail.com>
2013-02-18 15:09:18 -0500

    onSaveInstanceState method implemented

--- TestingApp/gen/com/example/testingapp/R.java
+++ TestingApp/gen/com/example/testingapp/R.java
@@ -14,7 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070002;
+        public static final int editText1=0x7f070002;
+        public static final int menu_settings=0x7f070003;
         public static final int my_textview=0x7f070000;
         public static final int my_button=0x7f070001;
     }
